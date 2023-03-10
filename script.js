427b23a40d6ec7942a57840915dd993090d5559b
Heron <kumar15-3985@diu.edu.bd>
2023-03-10 13:13:50 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+  strings: [
+    "Android App Developer",
+    "Flutter App Developer",
+    "Competitive Programmer",
+    "Web Developer",
+    "Freelancer",
+    "Problem Solver",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  cursorChar: " |",
+  smartBackspace: true,
+  loop: true,
+});
